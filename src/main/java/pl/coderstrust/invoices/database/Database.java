657120231dc6e8eaba7698e75e09d761f0b1fc80
pukacz <pawel.puchalski82@gmail.com>
2019-01-17 21:15:29 +0100

    Merge branch 'master' of github.com:CodersTrustPL/project-8-karolina-mariusz-pawel into File_Database_1
    # Conflicts:
    #	src/main/java/pl/coderstrust/invoices/model/Company.java
    #	src/main/java/pl/coderstrust/invoices/model/InvoiceEntry.java

--- src/main/java/pl/coderstrust/invoices/database/Database.java
+++ src/main/java/pl/coderstrust/invoices/database/Database.java
@@ -1,18 +1,20 @@
 package pl.coderstrust.invoices.database;
 
+import pl.coderstrust.invoices.model.Invoice;
+
 import java.time.LocalDate;
 import java.util.Collection;
-import pl.coderstrust.invoices.model.Invoice;
 
 public interface Database {
 
-    void saveInvoice(Invoice invoice);
+    void saveInvoice(Invoice invoice) throws DatabaseOperationException;
 
-    void deleteInvoice(Long id);
+    void deleteInvoice(Long id) throws DatabaseOperationException;
 
-    Invoice getInvoice(Long id);
+    Invoice getInvoice(Long id) throws DatabaseOperationException;
 
-    Collection<Invoice> getInvoices();
+    Collection<Invoice> getInvoices() throws DatabaseOperationException;
 
-    Collection<Invoice> getInvoicesByDate(LocalDate startDate, LocalDate endDate);
+    Collection<Invoice> getInvoicesByDate(LocalDate startDate, LocalDate endDate)
+            throws DatabaseOperationException;
 }
