6e3b0b5b58bcaff28fc66078154f1ba723bb7d28
pukacz <pawel.puchalski82@gmail.com>
2019-01-21 18:13:19 +0100

    -more constructors added

--- src/main/java/pl/coderstrust/invoices/database/DatabaseOperationException.java
+++ src/main/java/pl/coderstrust/invoices/database/DatabaseOperationException.java
@@ -5,4 +5,12 @@ public class DatabaseOperationException extends Exception {
     public DatabaseOperationException(String message) {
         super(message);
     }
+
+    public DatabaseOperationException() {
+        super();
+    }
+
+    public DatabaseOperationException(String message, Exception exception) {
+        super(message, exception);
+    }
 }
