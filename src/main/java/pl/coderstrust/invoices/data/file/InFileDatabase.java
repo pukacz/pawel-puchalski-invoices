cbd184bb7bc36b5ba82626b4158c3cdda84841e9
pukacz <pawel.puchalski82@gmail.com>
2019-01-24 00:11:17 +0100

    -more constructors added

--- src/main/java/pl/coderstrust/invoices/data/file/InFileDatabase.java
+++ src/main/java/pl/coderstrust/invoices/data/file/InFileDatabase.java
@@ -3,6 +3,7 @@ package pl.coderstrust.invoices.data.file;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -12,22 +13,37 @@ public class InFileDatabase {
 
     private FileHelper fileHelper;
     private ObjectMapper mapper;
+    private Configuration configuration;
 
     public InFileDatabase() {
+        //sprawdzamy czy plik invoices.dat istnieje, jesli nie to go tworzymy
         this.fileHelper = new FileHelper();
         mapper = new ObjectMapper();
+        configuration = new Configuration();
     }
 
     Collection<Invoice> getInvoices() throws IOException {
+
+        ArrayList<String> list = new ArrayList<>();
+        String line;
+
+        try (RandomAccessFile reader = new RandomAccessFile(configuration.getInvoicesFile(), "r")) {
+//            FileHelper fileHelper = new FileHelper(reader);
+            while ((line = reader.readLine()) != null) {
+                list.add(line);
+            }
+        }
+//        return list;
+
         List<String> lines = fileHelper.getInvoices();
         ArrayList<Invoice> invoices = new ArrayList<>();
         int colonPosition;
 
-        for (String line : lines) {
-            colonPosition = line.indexOf(":");
-            invoices.add(getInvoiceFromJSONString(line.substring(colonPosition)));
-        }
-        return invoices;
+//        for (String line : lines) {
+//            colonPosition = line.indexOf(":");
+//            invoices.add(getInvoiceFromJSONString(line.substring(colonPosition)));
+//        }
+        return null;
     }
 
 
@@ -36,7 +52,6 @@ public class InFileDatabase {
         String invoiceInJson = sentInvoiceToJSONString(invoice);
         fileHelper.saveInvoice(invoiceId, invoiceInJson);
     }
-
 
 //    void deleteInvoice(Long id) {
 //        if (isInvoiceExisting(id)) {
