92888f5b31c77fa398135e412ac7408de0757378
pukacz <pawel.puchalski82@gmail.com>
2019-01-24 00:11:17 +0100

    -more constructors added

--- src/main/java/pl/coderstrust/invoices/data/file/FileHelper.java
+++ src/main/java/pl/coderstrust/invoices/data/file/FileHelper.java
@@ -10,46 +10,30 @@ class FileHelper {
 
     public static void main(String[] args) throws IOException {
 
-//        File file = new File(new Configuration().getInvoicesFile());
-//        file.createNewFile();
-//
         FileHelper fileHelper = new FileHelper();
-//
-//        Company company1 = new Company(1L, "Krzak1", "111");
-//        Company company2 = new Company(2L, "Krzak2", "222");
-//
-//        Company company3 = new Company(3L, "Krzak3", "333");
-//        Company company4 = new Company(4L, "Krzak4", "444");
-//
-//        Invoice invoice1 = new Invoice(1111L, "issue1",
-//            LocalDate.of(2019, 1, 1), company1, company2,
-//            null);
-//
-//        Invoice invoice2 = new Invoice(3333L, "issue2",
-//            LocalDate.of(2019, 1, 2), company3, company4,
-//            null);
-//
-//        fileHelper.saveInvoice(invoice1.getId(), invoice1.toString());
-//        fileHelper.saveInvoice(invoice2.getId(), invoice1.toString());
 
-//        System.out.println(fileHelper.getPositionOfInvoice(2222L));
+//        String invoice1 = "1line";
+//        String invoice2 = "2line-zmienionyxxxx";
+//        String invoice3 = "3line";
 
-        String tekst =
-            "1111: Invoice{id=1111, issue='issue1', issueDate=2019-01-01, seller=Company{id=1, name='Krzak1', taxIdentificationNumber='111'}, buyer=Company{id=2, name='Krzak2', taxIdentificationNumber='222'}, entries=null}\n"
-                + "2222: Invoice{id=1111, issue='issue1', issueDate=2019-01-01, seller=Company{id=1, name='Krzak1', taxIdentificationNumber='111'}, buyer=Company{id=2, name='Krzak2', taxIdentificationNumber='222'}, entries=null}\n"
-                + "1111: Invoice{id=1111, issue='issue1', issueDate=2019-01-01, seller=Company{id=1, name='Krzak1', taxIdentificationNumber='111'}, buyer=Company{id=2, name='Krzak2', taxIdentificationNumber='222'}, entries=null}\n";
+//        fileHelper.saveInvoice(111L, invoice1);
+//        fileHelper.saveInvoice(222L, invoice2);
+//        fileHelper.saveInvoice(333L, invoice3);
 
-        int colonPosition = tekst.indexOf("2222: ");
-        int endOfLine = tekst.substring(colonPosition).indexOf("\n");
-//        System.out.println(endOfLine);
-        System.out.println(tekst.substring(endOfLine));
+//        System.out.println(fileHelper.getPositionOfInvoice(111L));
+//        System.out.println(fileHelper.getPositionOfInvoice(222L));
+//        System.out.println(fileHelper.getPositionOfInvoice(333L));
+        fileHelper.deleteInvoice(222L);
+        fileHelper.deleteInvoice(111L);
     }
 
     private String readOnly = "r";
     private String readWrite = "rw";
     private Configuration configuration;
+    private RandomAccessFile raf;
 
     FileHelper() {
+//        this.raf = raf;
         this.configuration = new Configuration();
     }
 
@@ -67,59 +51,79 @@ class FileHelper {
     }
 
     public void saveInvoice(Long invoiceId, String invoice) throws IOException {
+
         String line = "" + invoiceId + ": " + invoice + "\n";
-        Long position = getPositionOfInvoice(invoiceId);
+        Long cursor = getPositionOfInvoice(invoiceId);
 
         try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
             readWrite)) {
-            file.seek(position);
+            file.seek(cursor);
             file.writeBytes(line);
         }
     }
 
     public void deleteInvoice(Long invoiceId) throws IOException {
-        Long invoicePosition = getPositionOfInvoice(invoiceId);
-        if (invoicePosition.equals(0L)) {
 
-        } else {
-            try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
-                readWrite)) {
-                file.seek(invoicePosition);
-                file.readUnsignedByte();
+        try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
+            readWrite)) {
+
+            Long cursor = getPositionOfInvoice(invoiceId);
+            long remainingByteCount;
+            byte[] rest;
+
+            if (!cursor.equals(file.length())) {
+                file.seek(cursor);
+                String line = file.readLine();
+                remainingByteCount = file.length() - file.getFilePointer();
+
+                file.seek(file.getFilePointer() - line.getBytes().length-1);
+                for(int i=0; i<line.length();i++){
+                    file.writeBytes(" ");
+                }
+
+
+//                rest = new byte[(int) remainingByteCount];
+//                file.read(rest);
+
+//                file.seek(cursor);
+//                file.setLength(remainingByteCount);
+                file.writeBytes("\n");
+//                file.write(rest);
             }
-
         }
     }
 
     private Long getPositionOfInvoice(Long invoiceId) throws IOException {
-        long pointer = 0;
+        Long cursor = -1L;
 
         if (configuration.getInvoicesFile().equals(null)) {
             File file = new File(configuration.getInvoicesFile());
             file.createNewFile();
+            return cursor;
         }
 
         try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
             readOnly)) {
-            if (file.length() == 0) {
-                return 0L;
-            }
+
             String line;
-            int colonPosition;
-            Long invoiceInLineID;
+            String idInString;
+            int colonIndex;
+            cursor = file.getFilePointer();
 
             while ((line = file.readLine()) != null) {
-                colonPosition = line.indexOf(": ");
 
-                if (colonPosition > 0) {
-                    invoiceInLineID = Long.parseLong(line.substring(1, colonPosition));
-                    if (invoiceInLineID.equals(invoiceId)) {
-                        return pointer + colonPosition + 2;
+                if ((colonIndex = line.indexOf(": ")) > 0) {
+                    idInString = line.substring(0, colonIndex);
+
+                    if (invoiceId.equals(Long.parseLong(idInString))) {
+                        return cursor;
                     }
+                    cursor = file.getFilePointer();
+                } else {
+                    cursor += line.length() + 1;
                 }
-                pointer = file.getFilePointer();
             }
         }
-        return pointer;
+        return cursor;
     }
 }
