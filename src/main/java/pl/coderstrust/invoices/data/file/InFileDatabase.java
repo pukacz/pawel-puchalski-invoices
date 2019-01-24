bcf1ee1917efa99bce8f27adda17e07ffa4201e4
pukacz <pawel.puchalski82@gmail.com>
2019-01-24 21:23:31 +0100

    -methods added

--- src/main/java/pl/coderstrust/invoices/data/file/InFileDatabase.java
+++ src/main/java/pl/coderstrust/invoices/data/file/InFileDatabase.java
@@ -5,12 +5,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import pl.coderstrust.invoices.database.Database;
 import pl.coderstrust.invoices.model.Invoice;
 
-public class InFileDatabase {
+public class InFileDatabase implements Database {
+
+    public static void main(String[] args) throws IOException {
+        InFileDatabase fileDatabase = new InFileDatabase();
+    }
 
     private ObjectMapper mapper;
     private Configuration configuration;
@@ -19,13 +26,52 @@ public class InFileDatabase {
         mapper = new ObjectMapper();
         configuration = new Configuration();
 
-        if (configuration.getInvoicesFile().equals(null)) {
-            File file = new File(configuration.getInvoicesFile());
+        File file = new File(configuration.getInvoicesFile());
+        if (!file.exists()) {
             file.createNewFile();
         }
     }
 
-    Collection<Invoice> getInvoices() throws IOException {
+    @Override
+    public void saveInvoice(Invoice invoice) {
+        Long invoiceId = invoice.getId();
+        String invoiceInJson;
+
+        try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
+            "rw")) {
+            invoiceInJson = sentInvoiceToJsonString(invoice);
+            FileHelper fileHelper = new FileHelper(file);
+            fileHelper.saveInvoice(invoiceId, invoiceInJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteInvoice(Long invoiceId) {
+
+        try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
+            "rw")) {
+            FileHelper fileHelper = new FileHelper(file);
+            fileHelper.deleteInvoice(invoiceId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Invoice getInvoice(Long invoiceId) {
+        ArrayList<Invoice> invoices = new ArrayList<>(getInvoices());
+
+        for (Invoice invoice : invoices) {
+            if (invoice.getId().equals(invoiceId)) {
+                return invoice;
+            }
+        }
+        return null;
+    }
+
+    public Collection<Invoice> getInvoices() {
 
         ArrayList<Invoice> invoices = new ArrayList<>();
         List<String> lines;
@@ -39,39 +85,30 @@ public class InFileDatabase {
                 int colonPosition = line.indexOf(": ");
                 if (colonPosition > 0) {
                     invoiceInJson = line.substring(colonPosition);
-                    Invoice invoice = getInvoiceFromJSONString(invoiceInJson);
+                    Invoice invoice = getInvoiceFromJsonString(invoiceInJson);
                     invoices.add(invoice);
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return invoices;
     }
 
-    void saveInvoice(Invoice invoice) throws IOException {
-        Long invoiceId = invoice.getId();
-        String invoiceInJson = sentInvoiceToJSONString(invoice);
+    @Override
+    public Collection<Invoice> getInvoicesByDate(LocalDate startDate, LocalDate endDate) {
 
-        try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
-            "rw")) {
-            FileHelper fileHelper = new FileHelper(file);
-            fileHelper.saveInvoice(invoiceId, invoiceInJson);
-        }
+        return new ArrayList<>(getInvoices()).stream()
+            .filter(invoice -> invoice.getIssueDate().toEpochDay() >= startDate.toEpochDay())
+            .filter(invoice -> invoice.getIssueDate().toEpochDay() <= endDate.toEpochDay())
+            .collect(Collectors.toList());
     }
 
-    void deleteInvoice(Long invoiceId) throws IOException {
-
-        try (RandomAccessFile file = new RandomAccessFile(configuration.getInvoicesFile(),
-            "rw")) {
-            FileHelper fileHelper = new FileHelper(file);
-            fileHelper.deleteInvoice(invoiceId);
-        }
-    }
-
-    private Invoice getInvoiceFromJSONString(String line) throws IOException {
+    private Invoice getInvoiceFromJsonString(String line) throws IOException {
         return mapper.readValue(line, Invoice.class);
     }
 
-    private String sentInvoiceToJSONString(Invoice invoice) throws JsonProcessingException {
+    private String sentInvoiceToJsonString(Invoice invoice) throws JsonProcessingException {
         return mapper.writeValueAsString(invoice);
     }
 }
