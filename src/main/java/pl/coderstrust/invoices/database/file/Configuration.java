b2e433b1d370f10c359b310a68bfb7a66c147f65
pukacz <pawel.puchalski82@gmail.com>
2019-02-08 11:18:41 +0100

    -some updates

--- src/main/java/pl/coderstrust/invoices/database/file/Configuration.java
+++ src/main/java/pl/coderstrust/invoices/database/file/Configuration.java
@@ -4,11 +4,16 @@ import java.io.File;
 
 class Configuration {
 
-    private static final String INVOICES_FILE = "invoices.datxx";
+    private static final String INVOICES_FILE = "invoices.dat";
+    private static final String INVOICES_LAST_ID_FILE_COORDINATOR = "invoicesLastId.con";
     private static final String FOLDER = "src" + File.separator + "main" + File.separator
         + "resources" + File.separator;
 
     File getInvoicesFile() {
+        return new File(FOLDER + INVOICES_FILE);
+    }
+
+    File getInvoicesLastIdFile() {
         return new File(FOLDER + INVOICES_FILE);
     }
 }
