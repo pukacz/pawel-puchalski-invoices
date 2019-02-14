d813146a786678d2f3ed20a74487d1eda2a27f4a
pukacz <pawel.puchalski82@gmail.com>
2019-02-14 01:40:01 +0100

    -some updates

--- src/main/java/pl/coderstrust/invoices/database/file/InvoiceIdCoordinator.java
+++ src/main/java/pl/coderstrust/invoices/database/file/InvoiceIdCoordinator.java
@@ -8,7 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.TreeSet;
 
-public class InvoiceIdCoordinator {
+class InvoiceIdCoordinator {
 
     private File invoicesIdsFile;
     private TreeSet<Long> invoicesIds;
