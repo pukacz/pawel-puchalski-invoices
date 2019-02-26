94a8b4bdc4c6fbc3eabb9351867a1714272793c1
pukacz <pawel.puchalski82@gmail.com>
2019-02-26 01:04:57 +0100

    commit with errors

--- src/main/java/pl/coderstrust/invoices/database/file/InvoiceFileAccessor.java
+++ src/main/java/pl/coderstrust/invoices/database/file/InvoiceFileAccessor.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+public
 class InvoiceFileAccessor {
 
     private File invoicesFile;
